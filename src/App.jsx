4287d99e0e1dcabe2524df77c237744316f67490
alia jonathan <aliajonathan1@gmail.com>
2024-11-15 12:17:15 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptates adipisci in nemo reprehenderit expedita, facere velit consectetur inventore iusto, minus nam est, distinctio ad! Aut nesciunt voluptates consectetur incidunt!</p>
+    </>
+  )
+}
+
+export default App
